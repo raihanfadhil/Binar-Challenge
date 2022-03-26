fd1ab0fde01718c5855ab628c470be2d050c274d
Raihan Fadhil Ahmad <raihanfadhilahmad@gmail.com>
2022-03-26 06:36:25 +0000

    Update src/components/index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 import ListMovies from "./ListMovies"
 import LatestUploads from "./LatestUploads"
-import CardDetail from "./CardDetail/CardDetail"
 import ButtonIcon from "./ButtonIcon"
-export {ListMovies,LatestUploads,CardDetail,ButtonIcon}
+export {ListMovies,LatestUploads,ButtonIcon}
