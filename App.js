055966bcd2566f1b8ff8189d34e225a079d970eb
raihanfadhil <raihanfadhilahmad@gmail.com>
2022-04-27 00:38:50 +0700

    Crashlytics dan Cloud Messaging

---
+++ App.js
@@ -0,0 +1,84 @@
+import { StyleSheet, Text, View ,Button} from 'react-native'
+import React,{useEffect} from 'react'
+import crashlytics from '@react-native-firebase/crashlytics'
+import messaging from '@react-native-firebase/messaging';
+
+async function onSignIn(user) {
+  crashlytics().log('User signed in.');
+  await Promise.all([
+    crashlytics().setUserId(user.uid),
+    crashlytics().setAttribute('credits', String(user.credits)),
+    crashlytics().setAttributes({
+      role: 'admin',
+      followers: '13',
+      email: user.email,
+      username: user.username,
+    }),
+  ]);
+}
+
+const App = () => {
+  const getToken = async () => {
+    const token = await messaging().getToken()
+    alert(JSON.stringify(token))
+  }
+
+  const onNotificationOpen = () => {
+    //it will be trigger when app was in background
+    messaging().onNotificationOpenedApp(remoteMessage =>{
+      console.log(
+        'Notification caused app to open from background state',
+        remoteMessage.notification
+      )
+    })
+
+    messaging()
+      .getInitialNotification()
+      .then(remoteMessage => {
+        if(remoteMessage) {
+          console.log(
+            'Notification caused app to open from quit state:',
+            remoteMessage.notification
+          )
+
+        }
+      })
+    
+    messaging().onMessage(remoteMessage =>{
+      console.log(remoteMessage, 'On Message')
+    })
+  }
+
+  useEffect(() => {
+    onNotificationOpen()
+    getToken()
+    crashlytics().log('App mounted.')
+  }, []);
+
+  return (
+    <View>
+      <Text style={{fontWeight:"bold",textAlign:"center",color:"black",fontSize:25}}>Firebase Exercises </Text>
+      <View >
+      <Button
+        title="Sign In"
+        onPress={() =>
+          onSignIn({
+            uid: 'Aa0Bb1Cc2Dd3Ee4Ff5Gg6Hh7Ii8Jj9',
+            username: 'Joaquin Phoenix',
+            email: 'phoenix@example.com',
+            credits: 42,
+          })
+        }
+      />
+      <Button title="Test Crash" onPress={() => crashlytics().crash()} />
+
+
+
+      </View>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import messaging from '@react-native-firebase/messaging';
+
+messaging().setBackgroundMessageHandler(remoteMessage => {
+    console.log(remoteMessage, 'remote message from background')
+})
+AppRegistry.registerComponent(appName, () => App);
