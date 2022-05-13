0011296ad334a570c9ad23a7b951b458c5771ff0
Raihan Fadhil Ahmad <raihanfadhilahmad@gmail.com>
2022-05-13 13:57:45 +0000

    Update src/assets/Images/index.js

--- src/assets/Images/index.js
+++ src/assets/Images/index.js
@@ -1,8 +1,3 @@
-import Car from './Car.png'
-import CarXenia from './carXenia.png'
 import BG from './infinite.png'
-import Profile from './profile.png'
-import Allure from './Allure.png'
 
-
-export {Car, BG , Profile, CarXenia, Allure}
+export {BG}
